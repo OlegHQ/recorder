84c8935407ff638b6e8e5e0c427926825f4e7198
Oleg Pustovit <me@opustovit.com>
2026-09-18 17:06:05 +0200

    T-204 Global hotkeys

--- Sources/Recorder/App/AppDelegate.swift
+++ Sources/Recorder/App/AppDelegate.swift
@@ -18,6 +18,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
         wireProjects()
         wireOpen()
         wireFinishRecording()
+        Hotkeys.install()
         statusTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
             Task { @MainActor in self?.updateStatusItem() }
         }
